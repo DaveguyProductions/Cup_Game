78a4892743751b992e479a62f4a2cbb31ac725f0
DaveguyProductions <150758469+DaveguyProductions@users.noreply.github.com>
2025-11-06 19:59:47 -0800

    Godot Scripts
    Includes all current code from the main menu logic to the logic behind scene transitions

---
+++ Scripts/Menu/main_menu.gd
@@ -0,0 +1,237 @@
+extends Node2D
+####### NOTES ########
+# This is an area to write down the work I want to do next time I log in
+#
+# FIX MENU BUTTONS, I DONT THINK WE NEED TO CHECK A BOOL IF WE ARE DOING AN INT VAL PER SCREEN
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+# -------VARIABLES-------
+
+# Checks if escape was pressed during the opening 
+var menu_passed = false
+
+# Checks if the difficulty buttons are hidden
+var diff_hidden = true
+
+# Checks if the round buttons are hidden
+var rounds_hidden = true
+
+# Checks the difficulty value. Easy = 1. Normal = 2. Hard = 3.
+var difficulty = 0 
+
+# Checks the number of rounds Unlimited = 10. 
+var rounds = 0
+
+# Checks the screen you are on so that the back button takes you one screen back Main Menu = 0. Difficulty = 1. Rounds = 2. Settings = 3. Audio Settings = 4. Screen Settings = 5. Credits = 6. 
+var screen = 0 
+
+# -------PLAY BUTTON LOGIC-------
+func _on_button_mouse_entered() -> void:
+	$PlayButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Play_B2.png")
+	$Audio/ButtonHover.play()
+
+func _on_button_mouse_exited() -> void:
+	$PlayButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Play_B1.png")
+	
+func _on_play_button_pressed() -> void:
+	$Audio/ButtonClick.play()
+	hide_menu()
+	show_diff()
+	screen += 1
+
+# -------SETTINGS BUTTON LOGIC-------
+func _on_settings_button_mouse_entered() -> void:
+	$SettingsButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Settings_B2.png")
+	$Audio/ButtonHover.play()
+
+func _on_settings_button_mouse_exited() -> void:
+	$SettingsButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Settings_B1.png")
+	
+func _on_settings_button_pressed() -> void:
+	$Audio/ButtonClick.play()
+	$PlayButton.visible = false
+	$SettingsButton.visible = false
+	$BackButton.visible = true
+	$QuitButton.visible = false
+	$CupsRedMenu.visible = false
+
+# -------QUIT BUTTON LOGIC-------
+func _on_quit_button_mouse_entered() -> void:
+	$QuitButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Quit_B2.png")
+	$Audio/ButtonHover.play()
+
+func _on_quit_button_mouse_exited() -> void:
+	$QuitButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Quit_B1.png")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+	
+# -------EASY BUTTON SETTINGS -------
+func _on_easy_button_mouse_entered() -> void:
+	$Difficulty_Buttons/EasyButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Easy_B2.png")
+	$Audio/ButtonHover.play()
+	
+func _on_easy_button_mouse_exited() -> void:
+	$Difficulty_Buttons/EasyButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Easy_B1.png")
+
+func _on_easy_button_pressed() -> void:
+	difficulty = 1
+	screen = 2
+	hide_diff()
+	show_rounds()
+	$Audio/ButtonClick.play()
+
+# -------NORMAL BUTTON SETTINGS -------
+func _on_normal_button_mouse_entered() -> void:
+	$Difficulty_Buttons/NormalButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Normal_B2.png")
+	$Audio/ButtonHover.play()
+	
+func _on_normal_button_mouse_exited() -> void:
+	$Difficulty_Buttons/NormalButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Normal_B1.png")
+	
+func _on_normal_button_pressed() -> void:
+	difficulty = 2
+	screen = 2
+	hide_diff()
+	show_rounds()
+	$Audio/ButtonClick.play()
+
+# -------HARD BUTTON SETTINGS -------
+func _on_hard_button_mouse_entered() -> void:
+	$Difficulty_Buttons/HardButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Hard_B2.png")
+	$Audio/ButtonHover.play()
+	
+func _on_hard_button_mouse_exited() -> void:
+	$Difficulty_Buttons/HardButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Hard_B1.png")
+	
+func _on_hard_button_pressed() -> void:
+	difficulty = 3
+	screen = 2
+	hide_diff()
+	show_rounds()
+	$Audio/ButtonClick.play()
+
+# -------BACK BUTTON LOGIC-------
+func _on_back_button_mouse_entered() -> void:
+	$BackButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Back_B2.png")
+	$Audio/ButtonHover.play()
+
+func _on_back_button_mouse_exited() -> void:
+	$BackButton.icon = ResourceLoader.load("res://Assets/Art/Menu/Non-Rounds/Back_B1.png")
+	
+func _on_back_button_pressed() -> void:
+	if screen == 1 or screen == 3 or screen == 6:
+		show_menu()
+		$CupsRedMenu.visible = true
+	$Audio/ButtonClick.play()
+	if rounds_hidden == false:
+		hide_rounds()
+	if diff_hidden == false:
+		hide_diff()
+	
+# -------ROUND ONE BUTTON LOGIC-------
+func _on_round_1_button_mouse_entered() -> void:
+	$Round_Buttons/Round1Button.icon = ResourceLoader.load("res://Assets/Art/Menu/Rounds/One_Round_B2.png")
+	$Audio/ButtonHover.play()
+	rounds = 1
+
+func _on_round_1_button_mouse_exited() -> void:
+	$Round_Buttons/Round1Button.icon = ResourceLoader.load("res://Assets/Art/Menu/Rounds/One_Round_B1.png")
+
+# -------ROUND TWO BUTTON LOGIC-------
+
+# -------ROUND THREE BUTTON LOGIC-------
+
+# -------ROUND FOUR BUTTON LOGIC-------
+
+# -------ROUND FIVE BUTTON LOGIC-------
+
+# -------ROUND SIX BUTTON LOGIC-------
+
+# -------ROUND SEVEN BUTTON LOGIC-------
+
+# -------ROUND EIGHT BUTTON LOGIC-------
+
+# -------ROUND NINE BUTTON LOGIC-------
+
+# -------ROUND UNLIMITED BUTTON LOGIC-------
+
+
+# ------- FUNCTIONS -------
+func _on_canvas_layer_transition() -> void:
+	$DaveGuyProductionsSs.visible = false
+	$CanvasLayer/ColorRect.visible = false
+	$Audio/Menu_Music.play()
+	menu_passed = true
+
+func _input(event): 
+	if event.is_action_pressed("ui_cancel") and menu_passed == false:
+		$DaveGuyProductionsSs.visible = false
+		$CanvasLayer/ColorRect.visible = false
+		$Audio/Menu_Music.play()
+		$Audio/DaveGuyFanfare2.stop()
+		menu_passed = true
+
+func show_rounds():
+	$Round_Buttons/Round1Button.visible = true
+	$Round_Buttons/Round2Button.visible = true
+	$Round_Buttons/Round3Button.visible = true
+	$Round_Buttons/Round4Button.visible = true
+	$Round_Buttons/Round5Button.visible = true
+	$Round_Buttons/Round6Button.visible = true
+	$Round_Buttons/Round7Button.visible = true
+	$Round_Buttons/Round8Button.visible = true
+	$Round_Buttons/Round9Button.visible = true
+	$Round_Buttons/UnlimitedRoundButton.visible = true
+	rounds_hidden = false
+
+func hide_rounds():
+	$Round_Buttons/Round1Button.visible = false
+	$Round_Buttons/Round2Button.visible = false
+	$Round_Buttons/Round3Button.visible = false
+	$Round_Buttons/Round4Button.visible = false
+	$Round_Buttons/Round5Button.visible = false
+	$Round_Buttons/Round6Button.visible = false
+	$Round_Buttons/Round7Button.visible = false
+	$Round_Buttons/Round8Button.visible = false
+	$Round_Buttons/Round9Button.visible = false
+	$Round_Buttons/UnlimitedRoundButton.visible = false
+	rounds_hidden = true
+
+func show_diff():
+	$Difficulty_Buttons/EasyButton.visible = true
+	$Difficulty_Buttons/NormalButton.visible = true
+	$Difficulty_Buttons/HardButton.visible = true
+	diff_hidden = false
+
+func hide_diff():
+	$Difficulty_Buttons/EasyButton.visible = false
+	$Difficulty_Buttons/NormalButton.visible = false
+	$Difficulty_Buttons/HardButton.visible = false
+	diff_hidden = true
+
+func show_menu():
+	$PlayButton.visible = true
+	$SettingsButton.visible = true
+	$QuitButton.visible = true 
+	$BackButton.visible = false
+	screen = 0
+
+func hide_menu():
+	$PlayButton.visible = false
+	$SettingsButton.visible = false
+	$QuitButton.visible = false
+	$BackButton.visible = true
---
+++ Scripts/Menu/transition.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+signal transition 
+
+func _ready():
+	fade_to_clear()
+	
+func fade_to_clear():
+	$AnimationPlayer.play("fade_to_clear")
+	
+func fade_to_black():
+	$AnimationPlayer.play("fade_to_black")
+	
+
+
+func _on_dave_guy_fanfare_2_finished() -> void:
+	$AnimationPlayer.play("fade_to_black")
+	
+	
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "fade_to_black":
+		emit_signal("transition")
